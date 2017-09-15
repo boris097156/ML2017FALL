7fd7331287c43813cfb0c6a1edce8490eb16ceab
Boris Chen <boris097156@gmail.com>
2017-09-15 15:56:04 +0800

    complete hw0_2

---
+++ hw0/hw0_2.py
@@ -0,0 +1,13 @@
+import sys
+import math
+from PIL import Image
+
+old_im = Image.open(sys.argv[1])
+new_im = Image.new('RGB', old_im.size)
+
+width, height = old_im.size
+for w in range(width):
+	for h in range(height):
+		r,g,b = old_im.getpixel((w,h))
+		new_im.putpixel((w,h), (math.floor(r/2), math.floor(g/2), math.floor(b/2)))
+new_im.save('Q2.jpg')
