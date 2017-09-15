095e3aff2badfc8a1fa18a5fbefb546a8b15f5cd
Boris Chen <boris097156@gmail.com>
2017-09-15 16:00:38 +0800

    complete hw0_1

--- hw0/hw0_1.py
+++ hw0/hw0_1.py
@@ -2,11 +2,14 @@ import sys
 
 f = open(sys.argv[1], 'r')
 dic = {}
+output_name = 'Q1.txt'
+
 line = f.read().strip('\n').split()
 for word in line:
 	if word in dic:
 		dic[word] += 1
 	else:
 		dic[word] = 1
-for no, word in enumerate(dic):
-	print(word + ' ' + str(no) + '  ' + str(dic[word]))
+with open(output_name, 'w') as out:
+	for no, word in enumerate(dic):
+		out.write(word + ' ' + str(no) + '  ' + str(dic[word]) + '\n')
